d3111f7258e954da9b0be477303f38e2d5009b7f
Yash Pandey <yashdevesh10@gmail.com>
2024-03-22 11:25:17 +0530

    Created navbar component

--- 4. Mern-1/reactproject/src/App.jsx
+++ 4. Mern-1/reactproject/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
             {/* <SnackbarProvider>
                 <Snackbar />
             </SnackbarProvider> */}
-            <Hooks/>
+            {/* <Hooks/> */}
         </>
     );
 }
