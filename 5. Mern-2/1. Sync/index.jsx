ba8c23b7b468e32f8e8140cf69a3c1ea7622556a
Yash Pandey <yashdevesh10@gmail.com>
2024-04-01 12:00:00 +0530

    Adding Sync

---
+++ 5. Mern-2/1. Sync/index.jsx
@@ -0,0 +1,16 @@
+const fs = require("fs");
+// fs.writeFileSync("read.txt", "Welcome Everyone");
+
+// fs.appendFileSync("read.txt", "- Dark Batman");
+
+// const buf_data = fs.readFileSync("read.txt");
+// const data = buf_data.toString();
+// console.log(data);
+
+// const data1 = fs.readFileSync("read.txt", "utf-8");
+// console.log(data1);
+
+// fs.mkdirSync("Sample");
+// fs.writeFileSync("Sample/name.txt", "ahjskdashdiuashdid");
+// console.log(fs.readFileSync("Sample/name.txt", "utf-8"));
+fs.unlinkSync("Sample/name.txt");
