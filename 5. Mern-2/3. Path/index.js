2037c009fd092365cd0339cc8e41847cde24cb33
Yash Pandey <yashdevesh10@gmail.com>
2024-04-01 12:15:36 +0530

    Adding OS and Path

---
+++ 5. Mern-2/3. Path/index.js
@@ -0,0 +1,10 @@
+const os  = require("os");
+const path = require("path");
+
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.platform());
+// console.log(os.tmpdir());
+// console.log(os.type());
+
+console.log(path.parse("D:\Crio\5. Mern-2\3. Path\index.jsx"));
