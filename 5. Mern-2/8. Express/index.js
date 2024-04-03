7a0217067a786d026c259023e1699748fdeba02d
Yash Pandey <yashdevesh10@gmail.com>
2024-04-03 11:36:21 +0530

    Adding Views folder

--- 5. Mern-2/8. Express/index.js
+++ 5. Mern-2/8. Express/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const staticPath = path.join(__dirname, "./public");
+app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
     res.send("Home Page");
