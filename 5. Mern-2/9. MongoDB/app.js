f04e79574bfd4a74d94fd9fc51b0aa4f2523a5ce
Yash Pandey <yashdevesh10@gmail.com>
2024-04-09 12:32:23 +0530

    Adding app.js

--- 5. Mern-2/9. MongoDB/app.js
+++ 5. Mern-2/9. MongoDB/app.js
@@ -6,3 +6,23 @@ mongoose.connect("mongodb://127.0.0.1:27017/yash")
 }).catch((err) => {
     console.log(err);
 });
+
+const PlaylistSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    office : {
+        type : String,
+    },
+    age : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+const Playlist = new mongoose.model("playlist", PlaylistSchema);
